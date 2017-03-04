d4665011890835969c050e702d80efbeec93565f
Michael Borden <michael@quiet.space>
2017-03-03 16:27:55 -0800

    Add homepage and static routing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Web.Spock
@@ -9,20 +10,25 @@ import Data.Monoid
 import Data.IORef
 import qualified Data.Text as T
 
-data MySession = EmptySession
-data MyAppState = DummyAppState (IORef Int)
+import Network.Wai.Middleware.Static
+
+-- data MySession = EmptySession
+-- data MyAppState = DummyAppState (IORef Int)
 
 main :: IO ()
-main =
-    do ref <- newIORef 0
-       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
-       runSpock 8080 (spock spockCfg app)
+main = do 
+    -- ref <- newIORef 0
+    spockCfg <- defaultSpockCfg () PCNoDatabase ()
+    runSpock 8080 (spock spockCfg app)
 
-app :: SpockM () MySession MyAppState ()
-app =
-    do get root $
-           text "Hello World!"
-       get ("hello" <//> var) $ \name ->
-           do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+app :: SpockM () sess state ()
+app = do
+    cache <- liftIO $ initCaching PublicStaticCaching
+    middleware (staticPolicy' cache (hasPrefix "static")) -- Only allows reading from /static
+
+    get root $ file "text/html" "html/homepage.html"
+    
+    -- get ("hello" <//> var) $ \name -> do
+    --     (DummyAppState ref) <- getState
+    --     visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
+    --     text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
